e443da2a25303aa8305b1776b93350762ef3a500
Келсинбек <kelsinbek.zarlykov.bj@gmail.com>
2026-04-27 22:36:27 +0600

    initial project

--- src/test/java/kg/sportmanager/SportManagerApplicationTests.java
+++ src/test/java/kg/sportmanager/SportManagerApplicationTests.java
@@ -1,10 +1,10 @@
-package kg.sportnamager.sport_namager;
+package kg.sportmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SportNamagerApplicationTests {
+class SportManagerApplicationTests {
 
     @Test
     void contextLoads() {
