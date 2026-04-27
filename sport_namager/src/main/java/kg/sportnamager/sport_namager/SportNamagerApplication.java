477420cf48973aa50c1b7d0e6e86e26ec8efa5bb
Келсинбек <kelsinbek.zarlykov.bj@gmail.com>
2026-04-27 22:13:00 +0600

    initial project

---
+++ sport_namager/src/main/java/kg/sportnamager/sport_namager/SportNamagerApplication.java
@@ -0,0 +1,13 @@
+package kg.sportnamager.sport_namager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportNamagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportNamagerApplication.class, args);
+    }
+
+}
---
+++ sport_namager/src/test/java/kg/sportnamager/sport_namager/SportNamagerApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.sportnamager.sport_namager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportNamagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
