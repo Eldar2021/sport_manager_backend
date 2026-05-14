33f05867b56362575a91393c26c8d8bea5a34862
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-05-15 00:28:17 +0600

    feat(tests): add integration tests for managers API delete and list functionalities

---
+++ src/test/java/kg/sportmanager/managers/ManagersTestSupport.java
@@ -0,0 +1,10 @@
+package kg.sportmanager.managers;
+
+import kg.sportmanager.reports.ReportsTestSupport;
+
+/**
+ * Managers API integration testleri için ortak helper'lar.
+ * ReportsTestSupport'tan venue/table/session DB-builder'larını miras alır.
+ */
+public abstract class ManagersTestSupport extends ReportsTestSupport {
+}
